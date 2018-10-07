bba7d26b13f0b441e2e8a3f0c49db6e8c821a9f6
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-10-07 14:19:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,11 +2,20 @@
 
 def position_taken? (board,index)
   
+<<<<<<< HEAD
   if board[index] == "X" || board[index] == "O"
     return true
   elsif board[index] == "" || board[index] == " " || board[index] == nil
     return false
     
   end
+=======
+  if index == "X" || "O"
+    return true
+  else 
+    return false
+  end
+  
+>>>>>>> a59f63c16d1d5115d47a81c3d08beface6e949e3
 end
 
